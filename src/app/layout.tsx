00766c4e7aae72b38842c2e7284f535058a29886
liubov-vv <liubov-vv@users.noreply.github.com>
2026-03-27 16:43:00 +0200

    Add Google Analytics integration to RootLayout component.
    Incorporated Google Analytics tracking by adding necessary script tags to the RootLayout component. This includes loading the Google Tag Manager script and initializing the tracking configuration with the measurement ID.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -9,9 +10,25 @@ export const metadata: Metadata = {
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const gaMeasurementId = "G-NK8T28Z6HN";
+
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaMeasurementId}');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
